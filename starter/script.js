d4a650614aeb96048a66cab041a68b7f926c3220
Adeyemi <yemiklein77@gmail.com>
2023-01-06 11:38:31 +0100

    new update

--- starter/script.js
+++ starter/script.js
@@ -13,3 +13,37 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 let scores, currentScore, activePlayer, playing;
+
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+player0El.classList.remove('player--winner');
+player1El.classList.remove('player--winner');
+player0El.classList.add('player--active');
+player1El.classList.remove('player--active');
+
+
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    if (playing) {
+        //1. Generating a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
+    
+        //2. Display dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
+    
+        //3. Check for rolled 1
+        if (dice !== 1) {
+        //Add dice to current score
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+        //Switch to next player
+        switchPlayer();
+        }
+    }
+    }
